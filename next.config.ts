272e41005d7f761329cc4c89e8e8906de4d008e6
Alex Wilder <alexwilder.dev@gmail.com>
2026-07-05 15:46:00 +0100

    Add Sanity domains to CSP and images.remotePatterns

--- next.config.ts
+++ next.config.ts
@@ -12,8 +12,8 @@ const securityHeaders = [
       "script-src 'self' 'unsafe-inline' 'unsafe-eval'",
       "style-src 'self' 'unsafe-inline' https://fonts.googleapis.com",
       "font-src 'self' https://fonts.gstatic.com data:",
-      "img-src 'self' data: blob: https://marshharriercowley.co.uk",
-      "connect-src 'self' https://formspree.io",
+      "img-src 'self' data: blob: https://marshharriercowley.co.uk https://cdn.sanity.io",
+      "connect-src 'self' https://formspree.io https://api.sanity.io https://*.sanity.io wss://*.sanity.io",
       "frame-src https://www.google.com https://maps.google.com",
       "frame-ancestors 'none'",
     ].join("; "),
@@ -27,6 +27,10 @@ const nextConfig: NextConfig = {
       {
         protocol: "https",
         hostname: "marshharriercowley.co.uk",
+      },
+      {
+        protocol: "https",
+        hostname: "cdn.sanity.io",
       },
     ],
   },
