9c61a4c3da0833913046b5e51e52016de42499ba
Alex Wilder <alexwilder.dev@gmail.com>
2026-07-05 15:22:16 +0100

    Add initial Sanity configuration file

---
+++ sanity.config.ts
@@ -0,0 +1,13 @@
+import { defineConfig } from 'sanity'
+import { structureTool } from 'sanity/structure'
+import { schemaTypes } from './sanity/schemas'
+
+export default defineConfig({
+  name: 'marsh-harrier',
+  title: 'Marsh Harrier',
+  basePath: '/studio',
+  projectId: process.env.NEXT_PUBLIC_SANITY_PROJECT_ID!,
+  dataset: process.env.NEXT_PUBLIC_SANITY_DATASET!,
+  plugins: [structureTool()],
+  schema: { types: schemaTypes },
+})
